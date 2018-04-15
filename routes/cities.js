dfb49acb179fc668cac0ab05a0cde8a56c1f5d5a
Michael Wood <mwood@data-crush.com>
2018-04-15 10:34:07 -0400

    Added check for blank city name

--- routes/cities.js
+++ routes/cities.js
@@ -1,23 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync')
-var adapter = new FileSync('db.json')
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
 
 var db = low(adapter);
 
 /* GET cities listing. */
 router.get('/', function(req, res, next) {
   var name = new RegExp(req.query.name, 'i');
-  var city = db.get('cities')
-    .filter((item) => {
-      return item.name.match(name);
-    })
-    .sortBy('country')
-    .take(20)
-    .value();
+  if (req.query.name === '' || !req.query.name) {
+    res.send([]);
+  } else {
+    var city = db.get('cities')
+      .filter((item) => {
+        return item.name.match(name);
+      })
+      .sortBy('country')
+      .take(20)
+      .value();
 
-  res.send(city);
+    res.send(city);
+  }
 });
 
 module.exports = router;
